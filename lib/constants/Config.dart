1db5861539453972954661a6446ac914122de799
Yu Fang <yu@iotserv.com>
2021-03-03 17:37:25 +0800

    常量改为变量

--- lib/constants/Config.dart
+++ lib/constants/Config.dart
@@ -1,19 +1,19 @@
 class Config {
 //  serve web static file
-  static final String webStaticIp = "127.0.0.1";
-  static final int webStaticPort = 9000;
+  static String webStaticIp = "127.0.0.1";
+  static int webStaticPort = 9000;
 
-  static final String webgRpcIp = "127.0.0.1";
-  static final int webgRpcPort = 2080;
+  static String webgRpcIp = "127.0.0.1";
+  static int webgRpcPort = 2080;
 
 //http的api，目前主要是ssh的websocket
-  static final int webRestfulPort = 1081;
+  static int webRestfulPort = 1081;
 
-  static final String mdnsBaseTcpService = '_tcp.local.';
-  static final String mdnsBaseUdpService = '_udp.local.';
+  static String mdnsBaseTcpService = '_tcp.local.';
+  static String mdnsBaseUdpService = '_udp.local.';
 
-  static final String mdnsTypeExplorer = '_services._dns-sd._udp';
+  static String mdnsTypeExplorer = '_services._dns-sd._udp';
 
-  static final String mdnsIoTDeviceService = '_iotdevice._tcp';
-  static final String mdnsGatewayService = '_openiothub-gateway._tcp';
+  static String mdnsIoTDeviceService = '_iotdevice._tcp';
+  static String mdnsGatewayService = '_openiothub-gateway._tcp';
 }
