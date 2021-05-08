4027977a55118c217838d0d6c53b0cf841667e0b
Yu Fang <yu@iotserv.com>
2021-05-08 19:12:20 +0800

    添加微信SDK的配置

--- lib/constants/WeChatConfig.dart
+++ lib/constants/WeChatConfig.dart
@@ -1,2 +1,5 @@
-const String WECHAT_APPID = 'wx5c5302d3e6740145';
-const String WECHAT_UNIVERSAL_LINK = 'https://iothub.cloud/app/openiothub/'; // iOS 请配置
+
+class WeChatConfig {
+  static const String WECHAT_APPID = 'wx5c5302d3e6740145';
+  static const String WECHAT_UNIVERSAL_LINK = 'https://iothub.cloud/app/openiothub/'; // iOS 请配置
+}
--- lib/openiothub_constants.dart
+++ lib/openiothub_constants.dart
@@ -3,3 +3,4 @@ library openiothub_constants;
 export './constants/Config.dart';
 export './constants/Constants.dart';
 export './constants/SharedPreferences.dart';
+export './constants/WeChatConfig.dart';
