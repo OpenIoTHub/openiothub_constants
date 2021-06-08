2f92e24069a1f7e98a8957ba2e16a7b859d27c27
Yu Fang <yu@iotserv.com>
2021-06-08 17:13:48 +0800

    修改微信开放平台的id

--- lib/constants/WeChatConfig.dart
+++ lib/constants/WeChatConfig.dart
@@ -1,5 +1,5 @@
 
 class WeChatConfig {
-  static const String WECHAT_APPID = 'wx5c5302d3e6740145';
+  static const String WECHAT_APPID = 'wx9211939fcd68a318';
   static const String WECHAT_UNIVERSAL_LINK = 'https://iothub.cloud/app/openiothub/'; // iOS 请配置
 }
