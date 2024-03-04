b18e6f388995d0541569c90c290d8f9c6ca3efc3
Yu Fang <yu@iotserv.com>
2024-03-04 23:10:08 +0800

    git tag v1.0.1

--- lib/constants/Constants.dart
+++ lib/constants/Constants.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static final double ARROW_ICON_WIDTH = 16.0;
-  static final titleTextStyle = const TextStyle(fontSize: 16.0);
+  static final titleTextStyle = const TextStyle(fontSize: 16.0,
+      fontWeight: FontWeight.bold);
   static final rightArrowIcon = Image.asset(
     'assets/images/ic_arrow_right.png',
     width: ARROW_ICON_WIDTH,
