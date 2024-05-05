60d0b00f9e0bacee75af66ea43803ebf761628bf
iotserv <newfarry@126.com>
2024-05-05 20:51:03 +0800

    update

--- lib/constants/Constants.dart
+++ lib/constants/Constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
+  static final String DOC_WEBSITE_URL = "https://space.bilibili.com/1222749704";
   static final double ARROW_ICON_WIDTH = 16.0;
   static final titleTextStyle = const TextStyle(fontSize: 16.0,
       fontWeight: FontWeight.bold);
